f67029b967dade2e34262c8e8fdb6cf48088dc32
Arash Moradi <47105113+thearash@users.noreply.github.com>
2021-02-03 10:53:05 +0330

    .py updates

---
+++ history.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Thu Jul 30 16:45:07 2020)---
+runfile('H:/WithArash.py', wdir='H:')
+runfile('C:/Codes/WithArash.py', wdir='C:/Codes')
+runfile('C:/Codes/new.py', wdir='C:/Codes')
+
+## ---(Sun Jan 17 16:26:10 2021)---
+runfile('C:/Users/Arash/.spyder-py3/temp.py', wdir='C:/Users/Arash/.spyder-py3')
+runfile('C:/Users/Arash/.spyder-py3/temp.py', wdir='C:/Users/Arash/.spyder-py3')
+runfile('C:/Users/Arash/.spyder-py3/temp.py', wdir='C:/Users/Arash/.spyder-py3')
+runfile('C:/Users/Arash/.spyder-py3/IntroML.py', wdir='C:/Users/Arash/.spyder-py3')
+
+## ---(Wed Feb  3 10:42:40 2021)---
+runfile('C:/Users/Arash/.spyder-py3/IntroML.py', wdir='C:/Users/Arash/.spyder-py3')
